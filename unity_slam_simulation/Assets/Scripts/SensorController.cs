e181c6ec464ac89725b124e30ad82e051adb3bd8
Andrew Cheung <90213388+andrewcheung27@users.noreply.github.com>
2025-02-27 14:12:09 -0800

    changed sensor lines to use LineRenderer instead of Debug.DrawRay() so that it shows up when we build the game

--- unity_slam_simulation/Assets/Scripts/SensorController.cs
+++ unity_slam_simulation/Assets/Scripts/SensorController.cs
@@ -7,6 +7,7 @@ public class SensorController : MonoBehaviour
     public float sensorRange = 100f;
     public float maxSensorAngle = 60f;  // max angle of cone
     public int numSensorRays = 1000;  // number of raycasts to do when sensor is activated
+    public GameObject sensorLineRendererPrefab;  // empty GameObject with a LineRenderer component
     public float debugSensorRayDuration = 1f;  // how long the debug rays are shown for
 
     Vector3 SampleConeDirection(Vector3 direction, float angle)
@@ -29,9 +30,6 @@ public class SensorController : MonoBehaviour
         MeshRenderer meshRenderer;
         List<Point> pointCloud = new List<Point>();
 
-        // clear point cloud from previous sensor activation
-        pointCloud.Clear();
-
         Vector3 origin = transform.position;
         Vector3 forward = transform.forward;
 
@@ -47,13 +45,42 @@ public class SensorController : MonoBehaviour
             }
         }
 
-        // visualize raycasts
+        // visualize raycasts with Debug.DrawRay()
+        // if (debug) {
+        //     foreach (Point point in pointCloud) {
+        //         Debug.DrawRay(origin, point.position - origin, Color.red, debugSensorRayDuration);
+        //     }
+        // }
+
+        // visualize raycasts with LineRenderer
         if (debug) {
-            foreach (Point point in pointCloud) {
-                Debug.DrawRay(origin, point.position - origin, Color.red, debugSensorRayDuration);
-            }
+            StartCoroutine(DrawLines(pointCloud));
         }
 
         return pointCloud;
+    }
+
+    IEnumerator<WaitForSeconds> DrawLines(List<Point> pointCloud)
+    {
+        List<GameObject> lines = new List<GameObject>();
+
+        foreach(Point point in pointCloud) {
+            GameObject child = Instantiate(sensorLineRendererPrefab, transform);
+            lines.Add(child);
+            if (child.TryGetComponent(out LineRenderer renderer)) {
+                renderer.SetPosition(0, transform.position);
+                renderer.SetPosition(1, point.position);
+                renderer.startColor = Color.red;
+                renderer.endColor = Color.red;
+            }
+        }
+
+        yield return new WaitForSeconds(debugSensorRayDuration);
+
+        foreach(GameObject line in lines) {
+            if (line != null) {
+                Destroy(line);
+            }
+        }
     }
 }
