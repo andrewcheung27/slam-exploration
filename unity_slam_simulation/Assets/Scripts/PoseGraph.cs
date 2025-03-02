6d68019513f5a6027d47df1f2baa6afcc897eb25
Andrew Cheung <90213388+andrewcheung27@users.noreply.github.com>
2025-03-01 18:13:30 -0800

    cleaned up a bit

--- unity_slam_simulation/Assets/Scripts/PoseGraph.cs
+++ unity_slam_simulation/Assets/Scripts/PoseGraph.cs
@@ -13,6 +13,9 @@ public class PoseGraph
     private List<PoseNode> nodes;
     // maps pair of PoseNodes to a Pose, representing a spatial constraint between the nodes
     private Dictionary<Tuple<PoseNode, PoseNode>, Pose> constraints;
+    private int poseDimensions = 3;  // number of dimensions we're using for a Pose (3 dimensions of position, we aren't using rotation for simplicty)
+    private Matrix<double> normalEquationMatrix;  // H
+    private Vector<double> coefficientVector;  // b
 
     public PoseGraph(bool _debug=false)
     {
@@ -62,12 +65,6 @@ public class PoseGraph
         return Matrix<double>.Build.DenseOfArray(arr);
     }
 
-    public Matrix<double> ComputeJacobianBlock(PoseNode p1, PoseNode p2)
-    {
-        // shape 3x1
-        return null;
-    }
-
     public Tuple<Matrix<double>, Matrix<double>> ComputeJacobianBlocks(Pose p)
     {
         double[, ] arrA = {
@@ -86,39 +83,23 @@ public class PoseGraph
         return new Tuple<Matrix<double>, Matrix<double>>(A, B);
     }
 
-    // // takes a matrix and a smaller square block. puts the block in the matrix at start and end coordinates.
-    // // public void SetMatrixBlock(Matrix<double> matrix, Matrix<double> block, int startRow, int startCol, int endRow, int endCol)
-    // public void SetMatrixBlock(Matrix<double> matrix, Matrix<double> block, int rowMiddle, int colMiddle)
-    // {
-    //     int blockRow = 0;
-    //     int blockCol = 0;
-    //     int startRow = rowMiddle - block.SetSubMatrix
-
-    //     for (int row = startRow; row <= endRow; row++) {
-    //         for (int col = startCol; col <= endCol; col++) {
-    //             matrix[row, col] = block[blockRow, blockCol];
-    //             blockCol++;
-    //         }
-    //         blockRow++;
-    //         blockCol = 0;
-    //     }
-    // }
-
-    public Tuple<Matrix<double>, Vector<double>> BuildLinearSystem()
+    // mutates normalEquationMatrix and coefficientVector to build linear system
+    public void BuildLinearSystem()
     {
         // TODO: put information matrix somewhere else
         double[, ] omega = {{1.0, 2.0, 3.0}, {4.0, 5.0, 6.0}, {7.0, 8.0, 9.0}};
         var infoMatrix = Matrix<double>.Build.DenseOfArray(omega);
         // normal equation matrix
-        var H = Matrix<double>.Build.Sparse(100, 100);
+        normalEquationMatrix = Matrix<double>.Build.Sparse(100, 100);
         // coefficient vector
-        var b = Vector<double>.Build.Sparse(100, 1);
+        coefficientVector = Vector<double>.Build.Sparse(100, 1);
 
         // for each constraint:
         foreach (Tuple<PoseNode, PoseNode> nodePair in constraints.Keys) {
             // indices of the nodes
-            int i = nodePair.Item1.GetIndex();
-            int j = nodePair.Item2.GetIndex();
+            // TODO: explain multiply by 3
+            int i = nodePair.Item1.GetIndex() * poseDimensions;
+            int j = nodePair.Item2.GetIndex() * poseDimensions;
             // spatial constraint between the current pair of nodes
             Pose constraint = constraints[nodePair];
 
@@ -127,28 +108,17 @@ public class PoseGraph
 
             // compute blocks of Jacobian
             var (A, B) = ComputeJacobianBlocks(constraint);
-            // Matrix<double> jacobian_ii = ComputeJacobianBlock(constraint.Item1, constraint.Item1);
-            // Matrix<double> jacobian_ij = ComputeJacobianBlock(constraint.Item1, constraint.Item2);
-            // Matrix<double> jacobian_ji = ComputeJacobianBlock(constraint.Item2, constraint.Item1);
-            // Matrix<double> jacobian_jj = ComputeJacobianBlock(constraint.Item2, constraint.Item2);
 
             // update coefficient vector
-            // b[i] = (jacobian_ii.Transpose() * info_matrix * error)[0, 0];
-            // b[j] = (jacobian_jj.Transpose() * info_matrix * error)[0, 0];
-            // b[i * 3 + 0] = 
+            coefficientVector.SetSubVector(i, poseDimensions, (A.Transpose() * infoMatrix * error).Column(0));
+            coefficientVector.SetSubVector(j, poseDimensions, (B.Transpose() * infoMatrix * error).Column(0));
 
             // update normal equation matrix
-            // H[i, i] = (jacobian_ii.Transpose() * info_matrix * jacobian_ii)[0, 0];
-            // H[i, j] = (jacobian_ij.Transpose() * info_matrix * jacobian_ij)[0, 0];
-            // H[j, i] = (jacobian_ji.Transpose() * info_matrix * jacobian_ji)[0, 0];
-            // H[j, j] = (jacobian_jj.Transpose() * info_matrix * jacobian_jj)[0, 0];
-            H.SetSubMatrix(i - 1, i - 1, A.Transpose() * infoMatrix * A);
-            H.SetSubMatrix(i - 1, j - 1, A.Transpose() * infoMatrix * B);
-            H.SetSubMatrix(j - 1, i - 1, B.Transpose() * infoMatrix * A);
-            H.SetSubMatrix(j - 1, j - 1, B.Transpose() * infoMatrix * B);
+            normalEquationMatrix.SetSubMatrix(i, i, A.Transpose() * infoMatrix * A);
+            normalEquationMatrix.SetSubMatrix(i, j, A.Transpose() * infoMatrix * B);
+            normalEquationMatrix.SetSubMatrix(j, i, B.Transpose() * infoMatrix * A);
+            normalEquationMatrix.SetSubMatrix(j, j, B.Transpose() * infoMatrix * B);
         }
-
-        return new Tuple<Matrix<double>, Vector<double>>(H, b);
     }
 
     // TODO: implement pose graph optimization
@@ -157,12 +127,13 @@ public class PoseGraph
         // x is the pose graph
         bool converged = false;
 
-        // while (!converged)
         while (!converged) {
-            var (H, b) = BuildLinearSystem();
+            // build linear system
+            BuildLinearSystem();
+            var H = normalEquationMatrix;
+            var b = coefficientVector;
 
-            // deltaX = solveSparse(H * deltaX = -b)
-            // var deltaX = H.Solve(b);
+            // solve linear system
             Vector<double> deltaX = H.TransposeThisAndMultiply(H).Cholesky().Solve(H.TransposeThisAndMultiply(b));
 
             // x = x + deltaX
